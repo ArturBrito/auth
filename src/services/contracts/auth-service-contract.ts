d8a0be142595b9a54dd64013890cdf805c436875
arturbrito <artur.brito95@gmail.com>
2024-12-05 17:15:33 +0000

    fix: add missing methods to auth service interface

--- src/services/contracts/auth-service-contract.ts
+++ src/services/contracts/auth-service-contract.ts
@@ -2,4 +2,6 @@ import { AuthDto } from "../../domain/dto/auth-dto";
 
 export default interface IAuthService {
     signIn(email: string, password: string): Promise<AuthDto>;
+    refreshToken(refreshToken: string): Promise<AuthDto>;
+    signOut(refreshToken: string): Promise<void>;
 }
