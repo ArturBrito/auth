1fdaf5f12f9a321908c6a94f916db7d4500857f1
arturbrito <artur.brito95@gmail.com>
2024-12-17 16:59:13 +0000

    fix: only configure passport if GOOGLE_CLIENT_ID env variable is defined

--- src/api/loaders/passport.ts
+++ src/api/loaders/passport.ts
@@ -1,7 +1,10 @@
 import { Application } from 'express';
-import '../../infrastructure/passport/passport-config';
 import passport from 'passport';
 
 export default ({ app }: { app: Application }) => {
-    app.use(passport.initialize());
+    if (process.env.GOOGLE_CLIENT_ID) {
+        import('../../infrastructure/passport/passport-config').then(() => {
+            app.use(passport.initialize());
+        })
+    }
 }
